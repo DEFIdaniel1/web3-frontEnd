9d842f6843bd074810208071389e5eb11786466b
DEFIdaniel <greatprotectionwallet@gmail.com>
2022-08-03 15:31:02 -0600

    functions and buttons added for contractBalance, withdrawFunds

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
-import { ethers } from "./ethers-5.6.esm.min.js"
-import { fundMeAbi, contractAddress } from "./constants.js"
+import { ethers } from './ethers-5.6.esm.min.js'
+import { fundMeAbi, contractAddress } from './constants.js'
 
-const connectButton = document.getElementById("connectButton")
-const fundButton = document.getElementById("fundButton")
+const connectButton = document.getElementById('connectButton')
+const fundButton = document.getElementById('fundButton')
+const balanceButton = document.getElementById('balanceButton')
+const withdrawButton = document.getElementById('withdrawButton')
 connectButton.onclick = connect
 fundButton.onclick = fund
+balanceButton.onclick = getBalance
+withdrawButton.onclick = withdraw
 
 //connect to metamask
 async function connect() {
-    if (typeof window.ethereum !== "undefined") {
+    if (typeof window.ethereum !== 'undefined') {
         try {
-            await window.ethereum.request({ method: "eth_requestAccounts" })
-            connectButton.innerHTML = "Connected!"
+            await window.ethereum.request({ method: 'eth_requestAccounts' })
+            connectButton.innerHTML = 'Connected!'
         } catch (error) {
             console.log(error)
         }
-        const accounts = await ethereum.request({ method: "eth_accounts" })
+        const accounts = await ethereum.request({ method: 'eth_accounts' })
         console.log(accounts)
     } else {
-        connectButton.innerHTML = "Metamask not available."
+        connectButton.innerHTML = 'Metamask not available.'
     }
 }
 
 async function fund() {
-    const ethAmount = ethers.utils.parseEther("15")
+    const ethAmount = ethers.utils.parseEther(
+        document.getElementById('ethAmount').value
+    )
     console.log(`Funding with ${ethAmount}`)
 
-    if (typeof window.ethereum !== "undefined") {
+    if (typeof window.ethereum !== 'undefined') {
         const provider = new ethers.providers.Web3Provider(window.ethereum)
         const signer = provider.getSigner()
         const contract = new ethers.Contract(contractAddress, fundMeAbi, signer)
@@ -35,10 +41,12 @@ async function fund() {
                 value: ethAmount,
             })
             await listenForTransactionMine(fundTxResponse, provider)
-            console.log("Done")
+            console.log('Done')
         } catch (error) {
             console.log(error)
         }
+    } else {
+        fundButton.innerHTML = 'Metamask not available.'
     }
 }
 
@@ -55,6 +63,35 @@ function listenForTransactionMine(txResponse, provider) {
     })
 }
 
-//withdraw function
+async function withdraw() {
+    if (typeof window.ethereum !== 'undefined') {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, fundMeAbi, signer)
+        try {
+            const withdrawTxResponse = await contract.withdraw()
+            await listenForTransactionMine(withdrawTxResponse, provider)
+            console.log('Funds withdrawn successfully!')
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        withdrawButton.innerHTML = 'Metamask not available.'
+    }
+}
 
-//check contract balance
+async function getBalance() {
+    if (typeof window.ethereum !== 'undefined') {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        try {
+            const balance = await provider.getBalance(contractAddress)
+            const formattedBalance = ethers.utils.formatEther(balance)
+            console.log(formattedBalance)
+            balanceButton.innerHTML = `Balance is ${formattedBalance} ETH`
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        balanceButton.innerHTML = 'Metamask not available.'
+    }
+}
